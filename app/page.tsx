5a9c983b85ce39608c7078f2f338d875834b72a7
Wei Hung <wayne930242@gmail.com>
2023-07-31 16:20:24 +0800

    Remove the btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 'use client'
-import {
-  useState
-} from 'react'
+import { useState } from 'react'
 
 export default function Home() {
   const [query, setQuery] = useState('')
@@ -10,7 +8,7 @@ export default function Home() {
   async function createIndexAndEmbeddings() {
     try {
       const result = await fetch('/api/setup', {
-        method: "POST"
+        method: 'POST',
       })
       const json = await result.json()
       console.log('result: ', json)
@@ -24,8 +22,8 @@ export default function Home() {
     setLoading(true)
     try {
       const result = await fetch('/api/read', {
-        method: "POST",
-        body: JSON.stringify(query)
+        method: 'POST',
+        body: JSON.stringify(query),
       })
       const json = await result.json()
       setResult(json.data)
@@ -37,16 +35,20 @@ export default function Home() {
   }
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <input className='text-black px-2 py-1' onChange={e => setQuery(e.target.value)} />
-      <button className="px-7 py-1 rounded-2xl bg-white text-black mt-2 mb-2" onClick={sendQuery}>問 AI</button>
-      {
-        loading && <p>問 AI 關於 Dnd 的問題...</p>
-      }
-      {
-        result && <p>{result}</p>
-      }
-      { /* consider removing this button from the UI once the embeddings are created ... */}
-      <button onClick={createIndexAndEmbeddings}>建立索引並嵌入</button>
+      <input
+        className="text-black px-2 py-1"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button
+        className="px-7 py-1 rounded-2xl bg-white text-black mt-2 mb-2"
+        onClick={sendQuery}
+      >
+        問 AI
+      </button>
+      {loading && <p>問 AI 關於 Dnd 的問題...</p>}
+      {result && <p>{result}</p>}
+      {/* consider removing this button from the UI once the embeddings are created ... */}
+      {/* <button onClick={createIndexAndEmbeddings}>建立索引並嵌入</button> */}
     </main>
   )
 }
